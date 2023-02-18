7b8ba9caae5bff26100e3dd07583e7b03c097604
Alamin Arman <alamintm130205@gmail.com>
2023-02-18 20:55:06 +0600

    second commit

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,6 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const menuItems = (
+    <React.Fragment>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/">About</Link>
+      </li>
+      <li>
+        <Link to="/">Appointment</Link>
+      </li>
+      <li>
+        <Link to="/login">Reviews</Link>
+      </li>
+      <li>
+        <Link to="/login">Contact Us</Link>
+      </li>
+      <li>
+        <Link to="/login">Login</Link>
+      </li>
+    </React.Fragment>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -25,76 +48,15 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a href="/">Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a href="/" className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a href="/">Submenu 1</a>
-                </li>
-                <li>
-                  <a href="/">Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="/">Item 3</a>
-            </li>
+            {menuItems}
           </ul>
         </div>
-        <a href="/" className="btn btn-ghost normal-case text-xl">
-          daisyUI
-        </a>
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
+          Doctor's portal
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a href="/">Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a href="/">
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a href="/">Submenu 1</a>
-              </li>
-              <li>
-                <a href="/">Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a href="/">Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a href="/" className="btn">
-          Get started
-        </a>
+        <ul className="menu menu-horizontal px-1">{menuItems}</ul>
       </div>
     </div>
   );
