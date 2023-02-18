927555c07269fa47c2f30ed4d72c61f822c50173
Alamin Arman <alamintm130205@gmail.com>
2023-02-19 02:12:24 +0600

    banner part done

--- src/Pages/Home/Bannar/Banner.js
+++ src/Pages/Home/Bannar/Banner.js
@@ -1,25 +1,10 @@
 import React from "react";
+import banner from "../../../assets/images/Banner.png";
 
 const Banner = () => {
   return (
-    <div
-      className="hero"
-      style={{
-        backgroundImage: `url("")`,
-      }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn btn-primary">Get Started</button>
-        </div>
-      </div>
+    <div className="hero">
+      <img src={banner} alt="none" />
     </div>
   );
 };
